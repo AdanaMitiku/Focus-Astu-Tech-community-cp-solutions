7de7ea81ea03f5d7ffd353ce89991ce80ef2d80e
AdanaMitiku <adanamitiku2@gmail.com>
2026-03-11 20:29:09 +0300

    Update Team.py

--- Array/Team.py
+++ Array/Team.py
@@ -1,1 +1,13 @@
 
+n = int(input())  # number of problems
+count = 0
+
+for _ in range(n):
+    p, v, t = map(int, input().split())
+    if (p not in [0,1]) or (v not in [0,1]) or (t not in [0,1]):
+        print("invalid input")
+    if p + v + t >= 2:
+        count += 1
+        
+
+print(count)
