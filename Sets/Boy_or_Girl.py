7636433245e869daccaa01f8c485a0a5ac1610f1
AdanaMitiku <adanamitiku2@gmail.com>
2026-03-13 00:34:10 +0300

    Update Boy_or_Girl.py

--- Sets/Boy_or_Girl.py
+++ Sets/Boy_or_Girl.py
@@ -1,1 +1,10 @@
+username = input()
 
+if len(username) > 100:
+    print("Error: username too long")
+else:
+    distinct_count = len(set(username))
+    if distinct_count % 2 == 0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
