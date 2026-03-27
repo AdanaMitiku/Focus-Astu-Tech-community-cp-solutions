e13d64c93c2181b01687b02750fcccef0140ca1e
AdanaMitiku <adanamitiku2@gmail.com>
2026-03-27 11:58:04 +0300

    Update Registration_system.py

--- Hash_maps/Registration_system.py
+++ Hash_maps/Registration_system.py
@@ -1,1 +1,14 @@
 
+n = int(input())
+result = {}
+
+for _ in range(n):
+    name = input()
+    
+    if name in result
+        result[name] += 1
+        new_name = name + str(result[name])
+        print(new_name)
+    else:
+        result[name] = 0
+        print("OK")
